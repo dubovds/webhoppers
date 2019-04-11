7e3706943dd8d20fc9bc17bfc4c597204bd0e2ed
dubovds <dubovds87@gmail.com>
2019-04-11 14:15:31 +0300

    add left menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-/*----------------------------------------------------*/
-/*	Scroll Navbar
- /*----------------------------------------------------*/
+
+/* Scroll Navbar */
+
 $(window).scroll(function(){
 
   var b = $(window).scrollTop();
@@ -13,6 +13,15 @@ $(window).scroll(function(){
   }
 });
 
+
+/* toggle button */
+$(document).ready(function(){
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+});
+
+
 /* slow anhor */
 $(document).ready(function(){
   $(".goTo").on("click", function (event) {
@@ -24,21 +33,16 @@ $(document).ready(function(){
 });
 
 
-
-
+/* left menu */
 var lastId,
   topMenu = $("#left-menu"),
-  topMenuHeight = topMenu.outerHeight()-250,
-  // All list items
+  topMenuHeight = topMenu.outerHeight()-150,
   menuItems = topMenu.find("a"),
-  // Anchors corresponding to menu items
   scrollItems = menuItems.map(function(){
     var item = $($(this).attr("href"));
     if (item.length) { return item; }
   });
 
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
 menuItems.click(function(e){
   var href = $(this).attr("href"),
     offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
@@ -48,42 +52,20 @@ menuItems.click(function(e){
   e.preventDefault();
 });
 
-// Bind to scroll
 $(window).scroll(function(){
-  // Get container scroll position
   var fromTop = $(this).scrollTop()+topMenuHeight;
 
-  // Get id of current scroll item
   var cur = scrollItems.map(function(){
     if ($(this).offset().top < fromTop)
       return this;
   });
-  // Get the id of the current element
   cur = cur[cur.length-1];
   var id = cur && cur.length ? cur[0].id : "";
 
   if (lastId !== id) {
     lastId = id;
-    // Set/remove active class
     menuItems
       .parent().removeClass("active")
       .end().filter("[href='#"+id+"']").parent().addClass("active");
   }
-});
-
-
-
-
-
-
-$('.navbar-nav li a').click(function() {
-  $('#navigation-menu').css("height", "1px").removeClass("in").addClass("collapse");
-  $('#navigation-menu').removeClass("open");
-});
-
-
-$(document).ready(function(){
-  $('#nav-icon3').click(function(){
-    $(this).toggleClass('open');
-  });
 });
